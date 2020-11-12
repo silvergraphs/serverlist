baced44927d602b39663c2fa36d0eef0a8b183d7
Bruno Caruso <brunofcaruso@gmail.com>
2020-11-11 22:28:21 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,12 @@
 <?php
 
 // App configuration file
-define('DB_HOST', 'localhost'); // Database hostname
-define('DB_PORT', '8889'); // Database port
+define(
+  'DB_HOST',
+  'bfe083df6f1f60:58f87c8b@us-cdbr-east-02.cleardb.com/heroku_35c96611deb5761?reconnect=true'
+); // Database hostname
+define('DB_PORT', '3306'); // Database port
 define('DB_USER', 'root'); // Database username
-define('DB_PASS', 'root'); // Database password
+define('DB_PASS', ''); // Database password
 define('DB_NAME', 'serverlist'); // Database name
 define('TITLE', 'ServerList App'); // App name
