814c46bf7eb0451673fb1bb8a7ca50de69c6a398
Bruno Caruso <brunofcaruso@gmail.com>
2020-11-11 22:42:44 -0300

    Update config.php
    Modificado a un servidor MySQL externo

--- config.php
+++ config.php
@@ -1,12 +1,9 @@
 <?php
 
 // App configuration file
-define(
-  'DB_HOST',
-  'bfe083df6f1f60:58f87c8b@us-cdbr-east-02.cleardb.com/heroku_35c96611deb5761?reconnect=true'
-); // Database hostname
+define('DB_HOST', 'remotemysql.com'); // Database hostname
 define('DB_PORT', '3306'); // Database port
-define('DB_USER', 'bfe083df6f1f60'); // Database username
-define('DB_PASS', '58f87c8b'); // Database password
-define('DB_NAME', 'serverlist'); // Database name
+define('DB_USER', 'ju0Rm37joY'); // Database username
+define('DB_PASS', 'ugyrxYFXKv'); // Database password
+define('DB_NAME', 'ju0Rm37joY'); // Database name
 define('TITLE', 'ServerList App'); // App name
